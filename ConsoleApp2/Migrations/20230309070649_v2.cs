5c5ae5856ad1e88ab094a626ef64e472eaa4c636
user2 <user2@526-8>
2023-03-09 11:36:51 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Migrations/20230309070649_v2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ConsoleApp2.Migrations
+{
+    /// <inheritdoc />
+    public partial class v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "IdCategory", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("5cf656b3-d186-4062-b798-c5824d9a7002"), "Фрукты" },
+                    { new Guid("9aa9ae5e-615b-4cc8-8a7c-49947fc20718"), "Овощи" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "IdCategory",
+                keyValue: new Guid("5cf656b3-d186-4062-b798-c5824d9a7002"));
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "IdCategory",
+                keyValue: new Guid("9aa9ae5e-615b-4cc8-8a7c-49947fc20718"));
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleApp2.DB;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        private static GoodsContext _ef;
+       private static void Main(string[] args)
+        {
+            _ef = new GoodsContext();
+
+            _ef.Database.Migrate();
+
+            Console.WriteLine($"Connected to db : {_ef.Database.CanConnect()}");
+
+            foreach (var item in _ef.Categories.ToList())
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            var find_category = _ef.Categories.FirstOrDefault(
+                x => x.Name == "Снэки");
+            if (find_category == null)
+                find_category = new Category() { Name = "Снэки" };
+            foreach (var item in _ef.Goods.ToList())
+            {
+                double total_sale = item.Price - item.Sale;
+                Console.WriteLine($"{item.Name}" + $"Стоимость итоговая :{total_sale}");
+            }
+
+            //Good good = new Good()
+            //{
+            //    Name = "Чипсы",
+            //    Price = 199.0,
+            //    Sale = 0.50,
+            //    Category = find_category
+            //};
+        }
+    }
+}
